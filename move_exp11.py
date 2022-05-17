690555d8253234351aeaeff4dea55d6431be7847
SYJINTW <52921547+SYJINTW@users.noreply.github.com>
2022-05-18 01:31:16 +0800

    add exp11 results

---
+++ move_exp11.py
@@ -0,0 +1,23 @@
+import os
+from unittest import result
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import math
+from scipy import stats
+from pathlib import Path
+from statistics import mean
+
+def main():
+    dirPathGen = (Path('..')/'camera_placement_coverage'/'exp11').glob('idF5*')
+    savePath = Path('.')/'exp11'
+    savePath.mkdir(parents=True,exist_ok=True)
+    for dirPath in dirPathGen:
+        print(dirPath.name)
+        saveDirPath = (savePath/(dirPath.name))
+        saveDirPath.mkdir(parents=True,exist_ok=True)
+        copyDir = dirPath/'results'
+        os.system(f'powershell cp -r {copyDir} {saveDirPath}')
+
+if __name__ == '__main__':
+    main()
